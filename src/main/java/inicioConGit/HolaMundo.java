7b9ca3dff0047ae96e39695491904b0d7921ba5b
ajnebro <ajnebro@outlook.com>
2015-03-05 11:31:41 +0100

    Modified HolaMundo

--- src/main/java/inicioConGit/HolaMundo.java
+++ src/main/java/inicioConGit/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		int i = 0 ;
-        System.out.println("Hola, mundo, como te va?") ;
+        System.out.println("Hola, mmmmmmmmdoooooo, como te va?") ;
         System.out.println("Pues mira, por aqui andamos") ;
 
 	    System.out.println("asdfaasdfasdfsa");
