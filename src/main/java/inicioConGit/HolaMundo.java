2dbc42d4bfebf0ccb2c97b40f8c83ec929da29c3
ajnebro <ajnebro@outlook.com>
2015-03-05 11:22:14 +0100

    Added one sentence to HolaMundo

--- src/main/java/inicioConGit/HolaMundo.java
+++ src/main/java/inicioConGit/HolaMundo.java
@@ -3,6 +3,7 @@ package inicioConGit;
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		int i = 0 ;
         System.out.println("Hola, mundo, como te va?") ;
         System.out.println("Pues mira, por aqui andamos") ;
 	}
