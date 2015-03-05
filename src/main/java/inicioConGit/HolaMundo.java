e183baa9f1d72980eebd80553173456cc68d90bf
ajnebro <ajnebro@outlook.com>
2015-03-05 11:16:16 +0100

    Added HelloWorld, modified .gitignore and HolaMundo

--- src/main/java/inicioConGit/HolaMundo.java
+++ src/main/java/inicioConGit/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
         System.out.println("Hola, mundo, como te va?") ;
+        System.out.println("Pues mira, por aqui andamos") ;
 	}
 }
